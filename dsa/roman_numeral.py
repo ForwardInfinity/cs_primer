50ccdbce5c607c18b542028b85c738bb538c05c7
ForwardInfinity <huynguyen.workspace@gmail.com>
2025-10-10 02:00:02 +0700

    Revert "Fix assert in test case"
    This reverts commit a9d405c679a7da91b3b23ccbdcea2e6c5c8dc0f6.

--- dsa/roman_numeral.py
+++ dsa/roman_numeral.py
@@ -21,6 +21,16 @@ def to_roman(number):
 
 
 
+
+
+
+
+
+
+
+
+
+
 if __name__ == "__main__":
     test_cases = {
         39: "XXXIX",
@@ -30,6 +40,6 @@ if __name__ == "__main__":
     }
 
     for num, expected in test_cases.items():
-        assert to_roman(num) == expected, f"Wrong at {num}"
+        assert(num) == expected, f"Wrong at {num}"
 
     print("All tests passed!")
